6960bea25e860e4bb6b91d887dda7604d9dd1f0d
roy <barakroy@gmail.com>
2018-01-30 01:03:40 +0200

    First commit

---
+++ app/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','active',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function userActivationCode(){
+        return $this->hasOne(ActivationCode::class);
+    }
+
+    public function userIsActivated(){
+        if($this->active){
+            return true;
+        }
+
+        return true;
+    }
+
+
+}
